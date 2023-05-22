8bcd95386c807fa7abe82482e4a0004b072a3f9f
Kirill <kirill.orlov.kirill@mail.ru>
2023-05-22 18:28:08 +0300

    dz4 commit

--- hw1/dz1_dtmf.py
+++ hw1/dz1_dtmf.py
@@ -1,7 +1,6 @@
 import pyaudio as pa
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
 
 class Sound:
@@ -26,7 +25,7 @@ class Sound:
             "#": [941, 1477],
         }
 
-        self.fs = 10_000
+        self.fs = 44_100
         self.duration = 0.5
         self.p = pa.PyAudio()
         self.stream = self.p.open(format=pa.paFloat32, channels=1, rate=self.fs, output=True)
@@ -38,19 +37,18 @@ class Sound:
                 np.sin(np.outer(freq, 2 * np.pi * np.arange(self.fs * self.duration) / self.fs)),
                 axis=0).astype(np.float32).tobytes()
             self.stream.write(samples)
-            time.sleep(0.3)
 
         for ch in s:
             freq = self.freqs[ch]
-            x = np.linspace(0, 0.005, 100_000)
-            y = np.mean(np.sin(np.outer(freq, 2 * np.pi * x)), axis=0)
+            X = np.linspace(0, 0.005, 100_000)
+            y = np.mean(np.sin(np.outer(freq, 2 * np.pi * X)), axis=0)
             fig, ax = plt.subplots()
             ax.set_xlim(0, 0.005)
             ax.set_ylim(-1, 1)
             ax.set_xlabel("Time, s")
             ax.set_ylabel("Amplitudes")
             ax.set_title(f"{s} - {freq} Hz")
-            ax.plot(x, y)
+            ax.plot(X, y)
             plt.savefig(f"dz1_graphs/{s} - {freq} Hz.png")
 
 
